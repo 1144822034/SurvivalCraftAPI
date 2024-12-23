1c383550449f54601ba1a0a8e63e0861b20668fc
把红色赋予黑海∝ <txs20061216@126.com>
2024-12-23 20:23:35 +0800

    Revert "尝试暂缓社区不能清理内存问题"
    This reverts commit 2971110b9d6a80daf08097825b7c5270df34fa5a.

--- Survivalcraft/Screen/GameLoadingScreen.cs
+++ Survivalcraft/Screen/GameLoadingScreen.cs
@@ -1,9 +1,10 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 
 namespace Game
 {
 	public class GameLoadingScreen : Screen
-	{//加载存档时
+	{
 		public WorldInfo m_worldInfo;
 
 		public string m_worldSnapshotName;
@@ -97,7 +98,6 @@ namespace Game
 			m_worldSnapshotName = (string)parameters[1];
 			m_stateMachine.TransitionTo("WaitingForFadeIn");
 			ProgressManager.UpdateProgress("Loading World", 0f);
-			ScreensManager.m_screens.Remove("CommunityContent");
 		}
 	}
 }
