f434c2d1b84edbafcfb3119757d7006e56110f81
镁生素 <2094790738@qq.com>
2025-01-10 19:07:19 +0800

    关于时间和天气的一部分方法改为public访问，便于模组调整晨昏线等信息

--- Survivalcraft/Subsystem/SubsystemTimeOfDay.cs
+++ Survivalcraft/Subsystem/SubsystemTimeOfDay.cs
@@ -14,13 +14,13 @@ namespace Game
 
 		public float DayDuration { get; set; } = 1200f;
 
-		public float DawnStart { get; private set; }
+		public float DawnStart { get; set; }
 
-		public float DayStart { get; private set; }
+		public float DayStart { get; set; }
 
-		public float DuskStart { get; private set; }
+		public float DuskStart { get; set; }
 
-		public float NightStart { get; private set; }
+		public float NightStart { get; set; }
 
 		public float DayInterval => IntervalUtils.Interval(DayStart, DuskStart);
 
