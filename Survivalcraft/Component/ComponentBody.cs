8dfbc683144ea44418c88e5d03056a543d6468c9
镁生素 <2094790738@qq.com>
2025-01-15 20:58:08 +0800

    ComponentBody增加FixCollisionOnRidingBug开关

--- Survivalcraft/Component/ComponentBody.cs
+++ Survivalcraft/Component/ComponentBody.cs
@@ -205,7 +205,7 @@ namespace Game
 
 		public virtual Vector3 CollisionVelocityChange { get; set; }
 		public virtual bool CrouchPreventsFalling { get; set; } = true;
-
+		public virtual bool FixCollisionOnRidingBug { get; set; } = true;
 		public virtual BoundingBox BoundingBox
 		{
 			get
@@ -561,7 +561,7 @@ namespace Game
 					num7 -= num8;
 				}
 			}
-			if(ParentBody != null)
+			if(ParentBody != null && FixCollisionOnRidingBug)
 				CollisionVelocityChange = ParentBody.CollisionVelocityChange;
 			else
 				CollisionVelocityChange = m_velocity - velocity;
