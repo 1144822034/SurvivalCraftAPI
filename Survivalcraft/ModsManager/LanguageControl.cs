124d111118f3fe020e6ad931400a69ec02059235
LX.. <1659404657@qq.com>
2025-02-04 00:18:22 +0800

    还原MathUtils

--- Survivalcraft/ModsManager/LanguageControl.cs
+++ Survivalcraft/ModsManager/LanguageControl.cs
@@ -242,7 +242,7 @@ namespace Game
 				result = string.Empty;
 				return false;
 			}
-			string[] nm = blockName.Split([':'], StringSplitOptions.None);
+			string[] nm = blockName.Split(':');//这里不要改成集合表达式，在不同的编译器上会导致bug或者编译失败
 			result = Get(out bool r, "Blocks", nm.Length < 2 ? (blockName + ":0") : blockName, prop);
 			if (!r) result = Get(out r, "Blocks", nm[0] + ":0", prop);
 			return r;
