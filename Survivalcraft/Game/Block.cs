068249372908769517fdd3b69472e3381474d078
镁生素 <2094790738@qq.com>
2025-02-20 09:19:51 +0800

    方块的StaticBlockIndex保留为以前的变量，保证1.80mod兼容性。 可用UseConstIndex来控制

--- Survivalcraft/Game/Block.cs
+++ Survivalcraft/Game/Block.cs
@@ -27,7 +27,9 @@ namespace Game
 
 		public Vector3 FirstPersonOffset = Vector3.Zero;
 
-		public virtual bool StaticBlockIndex { get; set; } = false;
+		[Obsolete("overrode \"UseConstIndex\" instead.")]
+		public bool StaticBlockIndex = false;
+		public virtual bool UseConstIndex => StaticBlockIndex;
 
 		public bool CanBeBuiltIntoFurniture = false;
 
--- Survivalcraft/ModsManager/Changes/BlocksManager.cs
+++ Survivalcraft/ModsManager/Changes/BlocksManager.cs
@@ -307,7 +307,7 @@ namespace Game
                             Block = block,
                             Index = 0,
                             Allocated = false,
-                            StaticBlockIndex = (block.StaticBlockIndex || entity == ModsManager.SurvivalCraftModEntity || staticBlockIndexBefore),
+                            StaticBlockIndex = (block.UseConstIndex || entity == ModsManager.SurvivalCraftModEntity || staticBlockIndexBefore),
                             ModEntity = entity
                         });
                     }
