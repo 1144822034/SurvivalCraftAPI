09cd3b5fba82e2178627ab9cc9e05a689a361f36
销锋镝铸 <523084467@qq.com>
2025-02-28 00:28:40 +0800

    修复窗口模式的翻译和按F11全屏老恢复问题

--- Survivalcraft/Screen/SettingsUiScreen.cs
+++ Survivalcraft/Screen/SettingsUiScreen.cs
@@ -51,6 +51,10 @@ namespace Game
 			m_communityContentModeButton = Children.Find<ButtonWidget>("CommunityContentModeButton");
 			m_originalCommunityContentModeButton = Children.Find<ButtonWidget>("OriginalCommunityContentModeButton");
             m_deleteWorldNeedToTextButton = Children.Find<ButtonWidget>("DeleteWorldNeedToTextButton");
+            if(VersionsManager.PlatformString == "Android")
+            {
+	            m_windowModeButton.IsEnabled = false;
+            }
         }
 
 		public override void Enter(object[] parameters)
