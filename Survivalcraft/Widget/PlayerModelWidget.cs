9ff35234dafed8977ff8b63e3570d88be937d443
镁生素 <2094790738@qq.com>
2025-06-29 21:28:07 +0800

    人物模型改为每帧更新，以应对玩家模型随时变更

--- Survivalcraft/Widget/PlayerModelWidget.cs
+++ Survivalcraft/Widget/PlayerModelWidget.cs
@@ -144,22 +144,15 @@ namespace Game
 			}
 			m_modelWidget.ModelMatrix = (m_rotation != 0f) ? Matrix.CreateRotationY(m_rotation) : Matrix.Identity;
 		}
-		public bool m_modelsInitialized;
-		public virtual void InitializeModels()
+		public override void MeasureOverride(Vector2 parentAvailableSize)
 		{
+			if(OuterClothing) return;
+			if(m_modelWidget.Models.Contains(PlayerModel)) m_modelWidget.Models.Remove(PlayerModel);
+			if(m_modelWidget.Models.Contains(OuterClothingModel)) m_modelWidget.Models.Remove(OuterClothingModel);
 			OuterClothingModel = CharacterSkinsManager.GetOuterClothingModel(PlayerClass);
 			PlayerModel = CharacterSkinsManager.GetPlayerModel(PlayerClass);
 			m_modelWidget.AddModel(PlayerModel);
 			m_modelWidget.AddModel(OuterClothingModel);
-		}
-		public override void MeasureOverride(Vector2 parentAvailableSize)
-		{
-			if(OuterClothing) return;
-			if(!m_modelsInitialized)
-			{
-				m_modelsInitialized = true;
-				InitializeModels();
-			}
 			if (CameraShot == Shot.Body)
 			{
 				m_modelWidget.ViewPosition = (PlayerClass == PlayerClass.Male) ? new Vector3(0f, 1.46f, -3.2f) : new Vector3(0f, 1.39f, -3.04f);
