0d68fa50470628ef8e69ce321b9d8d45fdd4dab3
1144822034@qq.com <1144822034@qq.com>
2026-03-03 13:13:50 +0800

    [升级]IOS框架调整到10.0

--- SurvivalCraft.IOS/Main.cs
+++ SurvivalCraft.IOS/Main.cs
@@ -34,7 +34,7 @@ unsafe {
             var kitValue = Engine.Window.m_view.Native.UIKit.Value;
             var window = ObjCRuntime.Runtime.GetNSObject<UIWindow>(kitValue.Window);
             var uiView = window.RootViewController.View;
-            Engine.Window.RootViewController = uiView;
+            Engine.Window.RootViewController = window.RootViewController;
             uiView.Add(new TouchView(uiView));
         };
         Game.Program.EntryPoint();
